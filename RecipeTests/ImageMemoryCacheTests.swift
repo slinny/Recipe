137b2e3362ba096bab9d6c2f085d82bbcc405e65
Siran Li <siranli411@gmail.com>
2024-12-01 19:29:55 -0600

    Remove testTotalCostLimit from ImageMemoryCacheTests

--- RecipeTests/ImageMemoryCacheTests.swift
+++ RecipeTests/ImageMemoryCacheTests.swift
@@ -46,22 +46,4 @@ class ImageMemoryCacheTests: XCTestCase {
         // Then
         XCTAssertNil(cachedImage, "Fetching a non-existent image should return nil.")
     }
-
-    func testTotalCostLimit() {
-        // Given
-        let image = UIImage(systemName: "star")! // Sample image
-        let key = "largeImageKey"
-        
-        // Calculate the image size (in bytes)
-        let imageData = image.jpegData(compressionQuality: 1.0)!
-        
-        // Set the total cost limit to a value smaller than the size of the image
-        imageCache.cache(image, forKey: key)
-        
-        // When
-        let cachedImage = imageCache.fetch(forKey: key)
-        
-        // Then
-        XCTAssertNotNil(cachedImage, "The image should be cached.")
-    }
 }
---
+++ RecipeTests/RecipeListViewUITests.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeListViewUITests.swift
+//  RecipeTests
+//
+//  Created by Siran Li on 12/1/24.
+//
+
+import Foundation
